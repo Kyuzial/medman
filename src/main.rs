25af1fd3999bb7c24144b798ceec87980806f0c6
Kyuzial <kyuzial@protonmail.com>
2021-11-10 16:02:53 +0100

    Add store and read musicfiles

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use medman::cli::CliArguments;
 use medman::scan::scan;
+use std::io::{Write, Read};
 
 fn main() {
     let args = CliArguments::new();
     println!("{:?}", args);
+    match args.command().as_str() {
+        "scan" => {
+            let music_files = scan(args.path());
+            let mut file = std::fs::File::create("data.json").expect("create failed");
+            let save = serde_json::to_string_pretty(&music_files).unwrap();
+            file.write_all(save.as_bytes()).expect("Write failed");
+            for music_file in music_files {
+                println!("{:#?}", music_file);
+            }
+        },
+        "restore" => {
+            let mut file = std::fs::File::open(args.path()).unwrap();
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).unwrap();
+            print!("{}", contents);
+        },
+        _ => {panic!("No args")},
+    };
 
-    let music_files = scan(args.path());
-    for music_file in music_files {
-        println!("{:#?}", music_file);
-    }
 }
