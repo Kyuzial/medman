674ff0d4982eee0ba503805f53af9f2274b0b20b
Kyuzial <kyuzial@protonmail.com>
2021-11-17 16:39:59 +0100

    Restore remake struct instead of just printing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use medman::cli::CliArguments;
 use medman::scan::scan;
 use medman::search::search;
 use std::io::{Write, Read};
+use medman::musicfile::MusicFile;
+
 
 fn main() {
     let args = CliArguments::new();
@@ -10,17 +12,22 @@ fn main() {
         "scan" => {
             let music_files = scan(args.path());
             let mut file = std::fs::File::create("data.json").expect("create failed");
-            let save = serde_json::to_string_pretty(&music_files).unwrap();
+            let save = serde_json::to_string(&music_files).unwrap();
             file.write_all(save.as_bytes()).expect("Write failed");
             for music_file in music_files {
                 println!("{:#?}", music_file);
             }
         },
         "restore" => {
-            let mut file = std::fs::File::open(args.path()).expect("File doesn't exist!");
+            let mut file = std::fs::File::open(args.path()).expect("Couldn't open file");
             let mut contents = String::new();
             file.read_to_string(&mut contents).unwrap();
-            print!("{}", contents);
+
+            let mut music_files: Vec<MusicFile> = Vec::new();
+            music_files = serde_json::from_str(&contents).expect("Can't deserialize the file");
+            for music_file in music_files {
+                println!("{:#?}", music_file);
+            }
         },
         "search" => {
             
