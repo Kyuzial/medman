db7ebbf6ed46ec89f53a33d55be383f63c7b4430
Kyuzial <kyuzial@protonmail.com>
2021-11-20 23:45:25 +0100

    Markdown support

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,5 +1,3 @@
-use std::io::{Write, Read};
-use audiotags::AudioTagEdit;
 use markdown_gen::markdown::*;
 use crate::musicfile::MusicFile;
 
@@ -7,7 +5,11 @@ pub fn write2md(musicfiles: Vec<MusicFile>) {
     let file = std::fs::File::create("musicfiles.md").unwrap();
     let mut md = Markdown::new(file);
     for music_file in musicfiles {
-        md.write(music_file.title()).unwrap();
+        println!("{:#?}", music_file);
+        md.write(music_file.title().heading(1)).unwrap();
+        md.write(format!("Artist: {}", music_file.artist()).as_str()).unwrap();
+        md.write(format!("Album: {}", music_file.album()).as_str()).unwrap();
+        md.write(format!("Year: {}", music_file.year().to_string().as_str()).as_str()).unwrap();
     }
 }
 
