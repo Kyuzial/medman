e098fc942ab563cdcfa1debfb0d290f31ec04c83
Kyuzial <kyuzial@protonmail.com>
2021-11-22 14:31:43 +0100

    Tweak markdown gen

--- src/markdown.rs
+++ src/markdown.rs
@@ -5,10 +5,10 @@ pub fn write2md(musicfiles: Vec<MusicFile>) {
     let file = std::fs::File::create("musicfiles.md").unwrap();
     let mut md = Markdown::new(file);
     for music_file in musicfiles {
-        println!("{:#?}", music_file);
         md.write(music_file.title().heading(1)).unwrap();
         md.write(format!("Artist: {}", music_file.artist()).as_str()).unwrap();
         md.write(format!("Album: {}", music_file.album()).as_str()).unwrap();
         md.write(format!("Year: {}", music_file.year()).as_str()).unwrap();
+        md.write(format!("Path: {}", music_file.path()).as_str()).unwrap();
     }
 }
