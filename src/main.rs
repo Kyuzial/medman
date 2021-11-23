ed8e3acd4134b6937eeebad6cf57b53193eab6e8
Kyuzial <kyuzial@protonmail.com>
2021-11-23 23:05:40 +0100

    Add Read to interactive version

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,22 @@ fn main() {
                 };
             }
             if input.trim() == "read" {
+                println!("Which file do you want to read ?");
+                input.clear();
+                std::io::stdin()
+                .read_line(&mut input)
+                .expect("Can't read path");
+                let input_str = input.trim();
+                
+                let mut file = std::fs::File::open(input_str).expect("Couldn't open file");
+                let mut contents = String::new();
+                file.read_to_string(&mut contents).unwrap();
 
+                let mut music_files: Vec<MusicFile> = Vec::new();
+                music_files = serde_json::from_str(&contents).expect("Can't deserialize the file");
+                for music_file in music_files {
+                   println!("{:#?}", music_file);
+                }
             }
             if input.trim() == "search"{
 
